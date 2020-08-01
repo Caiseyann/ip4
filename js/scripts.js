95d764049744735cac7455e44155e2dfc8c2bda0
caiseyann <caiseyann4@gmail.com>
2020-08-01 08:48:58 +0300

    new function added

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ $(document).ready(function() {
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 });
+
+function myFunction() {
+    alert("To order fill the order box at the bottom of the page!");
+  }
