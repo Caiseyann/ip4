2cd71277b9b0936de33ae5fb3c42e32665c2677c
caiseyann <caiseyann4@gmail.com>
2020-08-02 09:21:22 +0300

    made new changes in the order section

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
 function myFunction() {
     alert("To order fill the order box at the bottom of the page!");
   }
+
+  $(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+        var pizza = $("input#pizzatype").val();
+        var topping = $("input#topping").val();
+        var crust = $("input#crusttype").val();
+        var quanity = $("input#quantity").val();
+        $("#pizza").text(pizzaInput);
+        $("#topping").text(toppingInput);
+        $("#crust").text(crustInput);
+        $("#quantity").text(quantityInput);
+
+        $("#story").show();
+
+        event.preventDefault();
+    });
+});
